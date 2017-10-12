bedb7e2d76d775805989ff3516a0fcf834f195d4
Klimchuk <andrei.klimchuk@vml.com.au>
2017-10-12 18:00:00 +1100

    lamda

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <vector>
 
 int main(int argc, char *argv[]) {
+    auto x = [=] { return argc; };
+    
     std::vector<int> numbers = { 1, 2, 3, 4 };
     for (int i : numbers) {
         std::cout << i << '\n';
     }
+    std::cout << x();
 }
